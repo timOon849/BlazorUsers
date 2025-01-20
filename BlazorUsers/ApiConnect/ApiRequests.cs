128389b84d31672bffccd361d5e012e0c83d516d
timOon849 <supersprait@gmail.com>
2025-01-20 11:38:21 +0300

    Добавьте файлы проекта.

---
+++ BlazorUsers/ApiConnect/ApiRequests.cs
@@ -0,0 +1,95 @@
+﻿using BlazorUsers.ApiConnect.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace BlazorUsers.ApiConnect
+{
+    public class ApiRequests
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILogger<ApiRequests> _logger;
+
+        public ApiRequests(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<UserData> GetAllUsersAsync()
+        {
+            var url = "api/UsersLogins/getAllUsers";
+
+            try
+            {
+                var response = await _httpClient.GetAsync(url).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+                if (string.IsNullOrEmpty(responseContent))
+                {
+                    _logger.LogWarning("Ответ от сервера пуст.");
+                    return new UserData();
+                }
+
+                var usersData = JsonSerializer.Deserialize<UserData>(responseContent, new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                });
+
+                return usersData ?? new UserData();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Ошибка при запросе");
+                return new UserData();
+            }
+        }
+
+        public async Task<UserAddData> AddNewUser(ReqDataUser user)
+        {
+            var url = "api/UsersLogins/createNewUserAndLogin";
+
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync(url, user);
+
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                var userAddData = JsonSerializer.Deserialize<UserAddData>(responseContent);
+
+                return userAddData ?? new UserAddData();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при запросе: {ex.Message}");
+                return new UserAddData();
+            }
+        }
+
+        public async Task<List<AuthorizationUser>> Authorization(string email, string pass)
+        {
+            var url = "api/UsersLogins/Authorization";
+
+            var loginData = new { email, pass };
+
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync(url, loginData);
+
+                response.EnsureSuccessStatusCode();
+
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                var authorizationResult = JsonSerializer.Deserialize<AuthorizationUser>(responseContent);
+                return [authorizationResult];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при запросе: {ex.Message}");
+                return [];
+            }
+        }
+    }
+}
---
+++ BlazorUsers/ApiConnect/Model/User.cs
@@ -0,0 +1,41 @@
+﻿namespace BlazorUsers.ApiConnect.Model
+{
+    public class UserDataShort
+    {
+        public int id_User { get; set; }
+        public string Name { get; set; }
+        public string AboutMe { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserData
+    {
+        public bool status { get; set; }
+        public UserDataContainer data { get; set; }
+    }
+
+    public class UserDataContainer
+    {
+        public List<UserDataShort> users { get; set; }
+    }
+
+    public class ReqDataUser
+    {
+        public string Name { get; set; }
+        public string AboutMe { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class AuthorizationUser
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class UserAddData
+    {
+        public bool status { get; set; }
+    }
+}
